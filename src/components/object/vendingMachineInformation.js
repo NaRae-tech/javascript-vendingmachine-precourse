7996dba54b9b269951c397b2707c388e8b1c6874
NaRae-tech <narae.dev@gmail.com>
2021-12-17 19:36:21 +0900

    자판기 객체 생성

---
+++ src/components/object/vendingMachineInformation.js
@@ -0,0 +1,17 @@
+export class vendingMachine{
+  constructor() {
+    this.itemList = [];
+    this.coinList = [{ 'FIVE-HUN': 0 }, { 'ONE-HUN': 0 }, { 'FIFTY': 0 }, { 'TEN': 0 }];
+  }
+  pushItem(item) {
+    this.itemList.push(item);
+  }
+  popItem(item) {
+    for (let i = 0; i < this.itemList.length; i++){
+      if (this.itemList[i].name === item.name) {
+        this.itemList.splice(i, 1);
+        break;
+      }
+    }
+  }
+}
