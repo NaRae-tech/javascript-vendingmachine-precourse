32b0a45d1808d8f2f0e213353d9697ff3bb5e9fa
NaRae-tech <narae.dev@gmail.com>
2021-12-17 21:43:21 +0900

    itemList속 중복 item 존재시, 수량만 증가

--- src/components/product-add/productAddButtonClicked.js
+++ src/components/product-add/productAddButtonClicked.js
@@ -1,16 +1,29 @@
 import { $ } from '../../dom.js';
 import { Item } from '../object/itemInformation.js';
+import { productAddInputValidity } from './productAddInputValidity.js';
 
-function makeInputsToItem() {
-  const productName = $('#product-name-input').value;
-  const productPrice = $('#product-price-input').value;
-  const productCount = $('#product-quantity-input').value;
-  const item = new Item(productName, productPrice, productCount);
-  return item;
+function duplicationValidity(name, price, count, itemList) {
+  for (let i = 0; i < itemList.length; i++){
+    if (itemList[i].name === name && itemList[i].price === price) {
+      return i;
+    }
+  }
+  return -1;
 }
 function productAdd() {
   const itemList = JSON.parse(localStorage.getItem('itemList'));
-  itemList.push(makeInputsToItem());
+  const productName = $('#product-name-input').value;
+  const productPrice = $('#product-price-input').value;
+  const productCount = $('#product-quantity-input').value;
+  const duplicationIndex = duplicationValidity(productName, productPrice, productCount, itemList);
+  if (duplicationIndex !== -1) {
+    itemList[duplicationIndex].count = parseInt(itemList[duplicationIndex].count) + parseInt(productCount);
+    console.log(itemList);
+  }
+  else if (productAddInputValidity(productName, productPrice, productCount)) {
+    const item = new Item(productName, productPrice, productCount);
+    itemList.push(item);
+  }
   localStorage.setItem('itemList', JSON.stringify(itemList));
 }
 export function ProductAddButtonClicked() {
