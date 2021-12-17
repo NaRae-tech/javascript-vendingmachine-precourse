d25e5944e1df961c7283855e6d5a161c04c17f9c
NaRae-tech <narae.dev@gmail.com>
2021-12-17 21:42:48 +0900

    input에 대한 유효성 검사후 alert창 출력

---
+++ src/components/product-add/productAddInputValidity.js
@@ -0,0 +1,40 @@
+function nameValidity(name) {
+  if (name.length === 0) {
+    return alert('이름은 1자 이상 입력해주세요.');
+  }
+  return true;
+}
+function priceValidity(price) {
+  if (isNaN(parseInt(price))) {
+    return alert('물품 가격은 숫자로 입력해주세요.');
+  }
+  if (price < 100) {
+    return alert('물품 가격은 100원이상이여야 합니다.');
+  }
+  if (price % 10 > 0) {
+    return alert('물품 가격은 10원이상 단위여야 합니다.');
+  }
+  return true;
+}
+function countValidity(count) {
+  if (isNaN(parseInt(count))) {
+    return alert('물품 개수는 숫자로 입력해주세요.');
+  }
+  if (count < 1) {
+    return alert('물품 개수는 1개 이상이여야 합니다.');
+  }
+  return true;
+}
+
+export function productAddInputValidity(name, price, count) {
+  if (!nameValidity(name)) {
+    return false;
+  }
+  if (!priceValidity(price)) {
+    return false;
+  }
+  if (!countValidity(count)) {
+    return false;
+  }
+  return true;
+}
