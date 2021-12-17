90e05409da0e6b87f10c3eaa104ec17cdffc3f2a
NaRae-tech <narae.dev@gmail.com>
2021-12-17 20:57:21 +0900

    localStorage에 없으면  vendingMachine 새로 생성

--- src/index.js
+++ src/index.js
@@ -1,25 +1,17 @@
-import { $ } from './dom.js';
-import { h1ElementCreate, buttonElementCreate, divElementCreate } from './components/makeElement.js';
-import ProductPurchaseMode from './components/product-purchase/productPurchaseMode.js';
-import MachineManageMode from './components/vending-machine-manage/vendingMachineManageMode.js';
-import ProductAddMode from './components/product-add/productAddMode.js';
+import { SetMainTitleButtons } from './components/mainTitleTab.js';
+import { VendingMachine } from './components/object/vendingMachineInformation.js';
 
-function setMainTitleButtons() {
-  const mainTitle = h1ElementCreate('🥤자판기🥤');
-  const purchaseTabButton = buttonElementCreate('product-purchase-menu', 'button', '상품 구매');
-  const manageTabButton = buttonElementCreate('vending-machine-manage-menu', 'button', '잔돈 충전');
-  const addTabButton = buttonElementCreate('product-add-menu', 'button', '상품 관리');
-  const contentDiv = divElementCreate('contents');
-  $('#app').append(mainTitle, purchaseTabButton, manageTabButton, addTabButton,contentDiv);
+function makeNewVendingMachine() {
+  let vendingMachine = new VendingMachine();
+  localStorage.setItem('vendingMachine', 'true');
+  localStorage.setItem('itemList', JSON.stringify(vendingMachine.itemList));
+  localStorage.setItem('coinList', JSON.stringify(vendingMachine.coinList));
 }
-function mainTabEventListeners(){
-  $('#product-purchase-menu').addEventListener('click', ProductPurchaseMode);
-  $('#vending-machine-manage-menu').addEventListener('click', MachineManageMode);
-  $('#product-add-menu').addEventListener('click', ProductAddMode);
-}
-
 export function vendingMachine() {
-  setMainTitleButtons();
-  mainTabEventListeners();
+  SetMainTitleButtons();
+  if (!localStorage.getItem('vendingMachine')) {
+    makeNewVendingMachine();
+  } 
 }
+
 vendingMachine();
